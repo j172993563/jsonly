6239c83f600bb5a9015c55f363b7dadcf8c9a7fe
roy <hdxzj@qq.com>
2026-06-17 06:21:54 +0800

    add: sign

---
+++ sign.js
@@ -0,0 +1,38 @@
+/**
+"longitude":113.33361952039931,"latitude":23.13224799262153,"deviceId":"812FC254-995D-48C2-9058-38E6A7873D41","altitude":"77.19913482666016","verticalAccuracy":"57.17357635498047"
+*/
+
+const req = $request
+let b = JSON.parse(req.body)
+const [c,d] = generate8DigitRandom(2)
+
+switch(b.type){
+	case 1:
+	  b.longitude = $persistentStore.read('longitude')*1
+		b.latitude = $persistentStore.read('latitude')*1
+		b.altitude = "40.199134" + c
+		b.verticalAccuracy = "57.173576" + d
+		break
+	case 2:
+	  b.longitude = Number('113.333619' + c)
+		$persistentStore.write(b.longitude, 'longitude')
+		b.latitude = Number('23.132247' + d)
+		$persistentStore.write(b.latitude, 'latitude')
+		break
+	default:
+	  $done({})
+}
+req.body = JSON.stringify(b)
+console.log(req)
+$done(req)
+
+function generate8DigitRandom(count = 1, len = 8) {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    // 生成 0 ~ 99999999 随机整数，转字符串后补 0 至 8 位
+    const randomNum = Math.floor(Math.random() * 100000000);
+    const DigitStr = randomNum.toString().padStart(len, '0');
+    result.push(DigitStr);
+  }
+  return result;
+}
